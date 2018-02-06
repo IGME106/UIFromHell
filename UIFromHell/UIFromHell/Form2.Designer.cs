45bc12c07d2967e9d40bcd8307a2a01d6099741c
Benjamin Kleynhans <kleynhansbenjamin@gmail.com>
2018-02-06 10:57:00 -0500

    Form 2 design and logic completed. comments outstanding

--- UIFromHell/UIFromHell/Form2.Designer.cs
+++ UIFromHell/UIFromHell/Form2.Designer.cs
@@ -28,128 +28,184 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.Form1CmbB1 = new System.Windows.Forms.ComboBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Form2AskPlainNameLbl = new System.Windows.Forms.Label();
+            this.Form2ExamplePlainLbl1 = new System.Windows.Forms.Label();
+            this.Form2ExampleEncryptedLbl1 = new System.Windows.Forms.Label();
+            this.Form2PlainNameTxtB = new System.Windows.Forms.TextBox();
+            this.Form2EncryptedNameTxtB = new System.Windows.Forms.TextBox();
+            this.Form2ExamplePlainLbl2 = new System.Windows.Forms.Label();
+            this.Form2ExampleEncryptedLbl2 = new System.Windows.Forms.Label();
+            this.Form2DirectionLbl = new System.Windows.Forms.Label();
+            this.Form2QtyLbl = new System.Windows.Forms.Label();
+            this.Form2DirectionCmbB = new System.Windows.Forms.ComboBox();
+            this.Form2QtyCmbB = new System.Windows.Forms.ComboBox();
+            this.Form2StateEncryptionLbl = new System.Windows.Forms.Label();
+            this.Form2EncryptedNameLbl = new System.Windows.Forms.Label();
+            this.Form2AskEncryptionTypeLbl = new System.Windows.Forms.Label();
+            this.Form2CaeserSubmitBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // Form2AskPlainNameLbl
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 74);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.Form2AskPlainNameLbl.Location = new System.Drawing.Point(28, 26);
+            this.Form2AskPlainNameLbl.Name = "Form2AskPlainNameLbl";
+            this.Form2AskPlainNameLbl.Size = new System.Drawing.Size(316, 17);
+            this.Form2AskPlainNameLbl.TabIndex = 0;
+            this.Form2AskPlainNameLbl.Text = "Enter your first name and press enter";
             // 
-            // label2
+            // Form2ExamplePlainLbl1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 218);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
+            this.Form2ExamplePlainLbl1.Location = new System.Drawing.Point(28, 185);
+            this.Form2ExamplePlainLbl1.Name = "Form2ExamplePlainLbl1";
+            this.Form2ExamplePlainLbl1.Size = new System.Drawing.Size(151, 17);
+            this.Form2ExamplePlainLbl1.TabIndex = 1;
+            this.Form2ExamplePlainLbl1.Text = "label2";
+            this.Form2ExamplePlainLbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Form2ExamplePlainLbl1.Visible = false;
             // 
-            // label3
+            // Form2ExampleEncryptedLbl1
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(47, 125);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(46, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "label3";
+            this.Form2ExampleEncryptedLbl1.Location = new System.Drawing.Point(28, 217);
+            this.Form2ExampleEncryptedLbl1.Name = "Form2ExampleEncryptedLbl1";
+            this.Form2ExampleEncryptedLbl1.Size = new System.Drawing.Size(151, 17);
+            this.Form2ExampleEncryptedLbl1.TabIndex = 2;
+            this.Form2ExampleEncryptedLbl1.Text = "label3";
+            this.Form2ExampleEncryptedLbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Form2ExampleEncryptedLbl1.Visible = false;
             // 
-            // textBox1
+            // Form2PlainNameTxtB
             // 
-            this.textBox1.Location = new System.Drawing.Point(173, 74);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 3;
+            this.Form2PlainNameTxtB.Location = new System.Drawing.Point(31, 52);
+            this.Form2PlainNameTxtB.Name = "Form2PlainNameTxtB";
+            this.Form2PlainNameTxtB.Size = new System.Drawing.Size(313, 22);
+            this.Form2PlainNameTxtB.TabIndex = 3;
+            this.Form2PlainNameTxtB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2ClearNameTxtB_KeyDown);
             // 
-            // textBox2
+            // Form2EncryptedNameTxtB
             // 
-            this.textBox2.Location = new System.Drawing.Point(139, 212);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 4;
+            this.Form2EncryptedNameTxtB.Location = new System.Drawing.Point(31, 310);
+            this.Form2EncryptedNameTxtB.Name = "Form2EncryptedNameTxtB";
+            this.Form2EncryptedNameTxtB.Size = new System.Drawing.Size(313, 22);
+            this.Form2EncryptedNameTxtB.TabIndex = 4;
+            this.Form2EncryptedNameTxtB.Visible = false;
+            this.Form2EncryptedNameTxtB.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2EncryptedNameTxtB_KeyDown);
             // 
-            // label4
+            // Form2ExamplePlainLbl2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(43, 159);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            this.Form2ExamplePlainLbl2.Location = new System.Drawing.Point(185, 185);
+            this.Form2ExamplePlainLbl2.Name = "Form2ExamplePlainLbl2";
+            this.Form2ExamplePlainLbl2.Size = new System.Drawing.Size(159, 17);
+            this.Form2ExamplePlainLbl2.TabIndex = 5;
+            this.Form2ExamplePlainLbl2.Text = "label4";
+            this.Form2ExamplePlainLbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Form2ExamplePlainLbl2.Visible = false;
             // 
-            // label5
+            // Form2ExampleEncryptedLbl2
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(27, 180);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 17);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "label5";
+            this.Form2ExampleEncryptedLbl2.Location = new System.Drawing.Point(185, 217);
+            this.Form2ExampleEncryptedLbl2.Name = "Form2ExampleEncryptedLbl2";
+            this.Form2ExampleEncryptedLbl2.Size = new System.Drawing.Size(159, 17);
+            this.Form2ExampleEncryptedLbl2.TabIndex = 6;
+            this.Form2ExampleEncryptedLbl2.Text = "label5";
+            this.Form2ExampleEncryptedLbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Form2ExampleEncryptedLbl2.Visible = false;
             // 
-            // label6
+            // Form2DirectionLbl
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(155, 159);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 17);
-            this.label6.TabIndex = 7;
-            this.label6.Text = "label6";
+            this.Form2DirectionLbl.Location = new System.Drawing.Point(28, 383);
+            this.Form2DirectionLbl.Name = "Form2DirectionLbl";
+            this.Form2DirectionLbl.Size = new System.Drawing.Size(43, 17);
+            this.Form2DirectionLbl.TabIndex = 7;
+            this.Form2DirectionLbl.Text = "Shift";
+            this.Form2DirectionLbl.Visible = false;
             // 
-            // label7
+            // Form2QtyLbl
             // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(166, 180);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(46, 17);
-            this.label7.TabIndex = 8;
-            this.label7.Text = "label7";
+            this.Form2QtyLbl.Location = new System.Drawing.Point(218, 383);
+            this.Form2QtyLbl.Name = "Form2QtyLbl";
+            this.Form2QtyLbl.Size = new System.Drawing.Size(25, 17);
+            this.Form2QtyLbl.TabIndex = 8;
+            this.Form2QtyLbl.Text = "by";
+            this.Form2QtyLbl.Visible = false;
             // 
-            // Form1CmbB1
+            // Form2DirectionCmbB
             // 
-            this.Form1CmbB1.FormattingEnabled = true;
-            this.Form1CmbB1.Location = new System.Drawing.Point(21, 269);
-            this.Form1CmbB1.Name = "Form1CmbB1";
-            this.Form1CmbB1.Size = new System.Drawing.Size(121, 24);
-            this.Form1CmbB1.TabIndex = 9;
+            this.Form2DirectionCmbB.FormattingEnabled = true;
+            this.Form2DirectionCmbB.Location = new System.Drawing.Point(77, 380);
+            this.Form2DirectionCmbB.Name = "Form2DirectionCmbB";
+            this.Form2DirectionCmbB.Size = new System.Drawing.Size(121, 24);
+            this.Form2DirectionCmbB.TabIndex = 9;
+            this.Form2DirectionCmbB.Visible = false;
             // 
-            // comboBox1
+            // Form2QtyCmbB
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(200, 267);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 10;
+            this.Form2QtyCmbB.FormattingEnabled = true;
+            this.Form2QtyCmbB.Location = new System.Drawing.Point(259, 380);
+            this.Form2QtyCmbB.Name = "Form2QtyCmbB";
+            this.Form2QtyCmbB.Size = new System.Drawing.Size(85, 24);
+            this.Form2QtyCmbB.TabIndex = 10;
+            this.Form2QtyCmbB.Visible = false;
+            // 
+            // Form2StateEncryptionLbl
+            // 
+            this.Form2StateEncryptionLbl.Location = new System.Drawing.Point(28, 92);
+            this.Form2StateEncryptionLbl.Name = "Form2StateEncryptionLbl";
+            this.Form2StateEncryptionLbl.Size = new System.Drawing.Size(316, 64);
+            this.Form2StateEncryptionLbl.TabIndex = 11;
+            this.Form2StateEncryptionLbl.Text = "Your name has been encrypted using a shift left/right Caesar cypher.Below are two" +
+    " shift index values to assist with your decryption.";
+            this.Form2StateEncryptionLbl.Visible = false;
+            // 
+            // Form2EncryptedNameLbl
+            // 
+            this.Form2EncryptedNameLbl.Location = new System.Drawing.Point(28, 258);
+            this.Form2EncryptedNameLbl.Name = "Form2EncryptedNameLbl";
+            this.Form2EncryptedNameLbl.Size = new System.Drawing.Size(316, 39);
+            this.Form2EncryptedNameLbl.TabIndex = 12;
+            this.Form2EncryptedNameLbl.Text = "Enter your first name in encrypted form and press enter";
+            this.Form2EncryptedNameLbl.Visible = false;
+            // 
+            // Form2AskEncryptionTypeLbl
+            // 
+            this.Form2AskEncryptionTypeLbl.Location = new System.Drawing.Point(28, 344);
+            this.Form2AskEncryptionTypeLbl.Name = "Form2AskEncryptionTypeLbl";
+            this.Form2AskEncryptionTypeLbl.Size = new System.Drawing.Size(316, 27);
+            this.Form2AskEncryptionTypeLbl.TabIndex = 13;
+            this.Form2AskEncryptionTypeLbl.Text = "What type of Caesar Cypher was used?";
+            this.Form2AskEncryptionTypeLbl.Visible = false;
+            // 
+            // Form2CaeserSubmitBtn
+            // 
+            this.Form2CaeserSubmitBtn.Enabled = false;
+            this.Form2CaeserSubmitBtn.Location = new System.Drawing.Point(31, 425);
+            this.Form2CaeserSubmitBtn.Name = "Form2CaeserSubmitBtn";
+            this.Form2CaeserSubmitBtn.Size = new System.Drawing.Size(313, 23);
+            this.Form2CaeserSubmitBtn.TabIndex = 14;
+            this.Form2CaeserSubmitBtn.Text = "Submit";
+            this.Form2CaeserSubmitBtn.UseVisualStyleBackColor = true;
+            this.Form2CaeserSubmitBtn.Visible = false;
+            this.Form2CaeserSubmitBtn.Click += new System.EventHandler(this.Form2CaeserSubmitBtn_Click);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(338, 340);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.Form1CmbB1);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(380, 474);
+            this.Controls.Add(this.Form2CaeserSubmitBtn);
+            this.Controls.Add(this.Form2AskEncryptionTypeLbl);
+            this.Controls.Add(this.Form2EncryptedNameLbl);
+            this.Controls.Add(this.Form2StateEncryptionLbl);
+            this.Controls.Add(this.Form2QtyCmbB);
+            this.Controls.Add(this.Form2DirectionCmbB);
+            this.Controls.Add(this.Form2QtyLbl);
+            this.Controls.Add(this.Form2DirectionLbl);
+            this.Controls.Add(this.Form2ExampleEncryptedLbl2);
+            this.Controls.Add(this.Form2ExamplePlainLbl2);
+            this.Controls.Add(this.Form2EncryptedNameTxtB);
+            this.Controls.Add(this.Form2PlainNameTxtB);
+            this.Controls.Add(this.Form2ExampleEncryptedLbl1);
+            this.Controls.Add(this.Form2ExamplePlainLbl1);
+            this.Controls.Add(this.Form2AskPlainNameLbl);
             this.Name = "Form2";
             this.Text = "Form2";
             this.ResumeLayout(false);
@@ -159,16 +215,20 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.ComboBox Form1CmbB1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label Form2AskPlainNameLbl;
+        private System.Windows.Forms.Label Form2ExamplePlainLbl1;
+        private System.Windows.Forms.Label Form2ExampleEncryptedLbl1;
+        private System.Windows.Forms.TextBox Form2PlainNameTxtB;
+        private System.Windows.Forms.TextBox Form2EncryptedNameTxtB;
+        private System.Windows.Forms.Label Form2ExamplePlainLbl2;
+        private System.Windows.Forms.Label Form2ExampleEncryptedLbl2;
+        private System.Windows.Forms.Label Form2DirectionLbl;
+        private System.Windows.Forms.Label Form2QtyLbl;
+        private System.Windows.Forms.ComboBox Form2DirectionCmbB;
+        private System.Windows.Forms.ComboBox Form2QtyCmbB;
+        private System.Windows.Forms.Label Form2StateEncryptionLbl;
+        private System.Windows.Forms.Label Form2EncryptedNameLbl;
+        private System.Windows.Forms.Label Form2AskEncryptionTypeLbl;
+        private System.Windows.Forms.Button Form2CaeserSubmitBtn;
     }
 }
--- UIFromHell/UIFromHell/Form2.cs
+++ UIFromHell/UIFromHell/Form2.cs
@@ -12,9 +12,191 @@ namespace UIFromHell
 {
     public partial class Form2 : Form
     {
+        Random random = new Random();
+
+        private int encryptionShiftBy = 0;
+        private string encryptionShiftDirection = "right";
+
+        private string clearName = string.Empty;
+
+        private string encryptedName = string.Empty;
+
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void Form2ClearNameTxtB_KeyDown(object sender, KeyEventArgs e)
+        {
+            encryptionShiftBy = random.Next(0, 26);
+            clearName = Form2PlainNameTxtB.Text;
+            
+            if (e.KeyCode == Keys.Return)
+            {
+                EncryptString();                
+            }
+        }
+
+        private void EncryptString()
+        {
+            encryptedName = Cryptograph("encipher", clearName, encryptionShiftBy);
+
+            UpdateDisplaySecondPhase();
+        }
+
+        private void DecryptString()
+        {
+
+        }
+
+        private char Cipher(string direction, char ch, int key)
+        {
+            char returnValue = ' ';
+
+            if (!char.IsLetter(ch))
+            {
+                returnValue = ch;
+            }
+            else
+            {
+                char d = char.IsUpper(ch) ? 'A' : 'a';
+
+                returnValue = (char)((((ch + key) - d) % 26) + d);
+            }
+
+            return returnValue;
+        }
+
+        private string Cryptograph(string action, string input, int key)
+        {
+            string returnValue = String.Empty;
+
+            switch (action)
+            {
+                case "encipher":
+                    foreach (char ch in input)
+                        returnValue += Cipher(encryptionShiftDirection, ch, key);
+
+                    break;
+                case "decipher":
+                    returnValue = Cryptograph("encipher", input, 26 - key);
+
+                    break;
+                default:
+                    break;
+            }
+
+            return returnValue;
+        }
+
+        private void UpdateDisplaySecondPhase()
+        {
+            Form2StateEncryptionLbl.Enabled = true;
+            Form2StateEncryptionLbl.Visible = true;
+
+            Form2ExamplePlainLbl1.Text = "ABC";
+            Form2ExampleEncryptedLbl1.Text = Cryptograph(
+                                                    "encipher", 
+                                                    Form2ExamplePlainLbl1.Text,
+                                                    encryptionShiftBy
+                                                );
+
+            Form2ExamplePlainLbl2.Text = "GIK";
+            Form2ExampleEncryptedLbl2.Text = Cryptograph(
+                                                    "encipher",
+                                                    Form2ExamplePlainLbl2.Text,
+                                                    encryptionShiftBy
+                                                );
+
+            Form2ExamplePlainLbl1.Enabled = true;
+            Form2ExamplePlainLbl1.Visible = true;
+            Form2ExampleEncryptedLbl1.Enabled = true;
+            Form2ExampleEncryptedLbl1.Visible = true;
+
+            Form2ExamplePlainLbl2.Enabled = true;
+            Form2ExamplePlainLbl2.Visible = true;
+            Form2ExampleEncryptedLbl2.Enabled = true;
+            Form2ExampleEncryptedLbl2.Visible = true;
+
+            Form2EncryptedNameLbl.Enabled = true;
+            Form2EncryptedNameLbl.Visible = true;
+
+            Form2EncryptedNameTxtB.Enabled = true;
+            Form2EncryptedNameTxtB.Visible = true;
+        }
+
+        private void Form2EncryptedNameTxtB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                if (IsCorrectValue())
+                {
+                    DisplayLastElements();
+                }
+            }            
+        }
+
+        private bool IsCorrectValue()
+        {
+            bool returnValue = false;
+
+            if (Form2EncryptedNameTxtB.Text.Equals(encryptedName)) {
+                returnValue = true;
+            }
+
+            return returnValue;
+        }
+
+        private void DisplayLastElements()
+        {
+            Form2AskEncryptionTypeLbl.Enabled = true;
+            Form2AskEncryptionTypeLbl.Visible = true;
+
+            Form2DirectionLbl.Enabled = true;
+            Form2DirectionLbl.Visible = true;
+
+            Form2DirectionCmbB.Items.Add("left");
+            Form2DirectionCmbB.Items.Add("right");
+
+            Form2DirectionCmbB.Enabled = true;
+            Form2DirectionCmbB.Visible = true;
+
+            for (int i = 0; i < 27; i++)
+            {
+                Form2QtyCmbB.Items.Add(i);
+            }
+
+            Form2QtyLbl.Enabled = true;
+            Form2QtyLbl.Visible = true;
+
+            Form2QtyCmbB.Enabled = true;
+            Form2QtyCmbB.Visible = true;
+
+            Form2CaeserSubmitBtn.Enabled = true;
+            Form2CaeserSubmitBtn.Visible = true;
+        }
+        
+        private void Form2CaeserSubmitBtn_Click(object sender, EventArgs e)
+        {
+            if ((Form2DirectionCmbB.Text.Equals(encryptionShiftDirection)) &&
+                    ((Form2QtyCmbB.Text.Equals(encryptionShiftBy.ToString()))))
+            {
+                this.Hide();
+
+                Form3 form3 = new Form3();
+                DialogResult form3Result = form3.ShowDialog(this);
+
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show(
+                owner: this,
+                text: "Sorry, that is wrong",
+                caption: "You made a Boo-boo",
+                buttons: MessageBoxButtons.OK
+                );
+            }
         }
     }
 }
